c621ef6eb088acb661b359186ca0dc0eebcb3eff
caleb dickson <caleb.n.dickson@gmail.com>
2024-12-25 09:35:46 -0800

    all fixes

--- src/pages/spreadsheet/Spreadsheet.jsx
+++ src/pages/spreadsheet/Spreadsheet.jsx
@@ -1,10 +1,25 @@
 import React from "react";
+import "./spreadsheet.css";
 
-const Spreadsheet = () => {
+const Spreadsheet = ({ spreadsheetId }) => {
+  const spreadsheetUrl = `https://docs.google.com/spreadsheets/d/${spreadsheetId}/edit?gid=0#gid=0`;
+
   return (
-    <div>
-      <h1>Spreadsheet</h1>
-      {/* Spreadsheet content */}
+    <div
+      style={{ textAlign: "center", marginTop: "20px", width: "max-content" }}>
+      <iframe
+        src={spreadsheetUrl}
+        width="1000px"
+        height="800"
+        frameBorder="0"
+        style={{ border: "1px solid #ccc" }}
+        title="Embedded Spreadsheet"></iframe>
+
+      <div style={{ marginTop: "10px" }}>
+        <a href={spreadsheetUrl} target="_blank" rel="noopener noreferrer">
+          Spreadsheet in New Tab
+        </a>
+      </div>
     </div>
   );
 };
